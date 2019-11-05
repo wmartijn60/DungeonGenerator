9c52471084ee16a2aa9bd22c129f94c2d6b51671
wmartijn60 <34513939+wmartijn60@users.noreply.github.com>
2019-11-05 15:07:39 +0100

    Last Add
    last github corupted so this is the new page

---
+++ Procedurally Generated Dungeon/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //   int randStartingPos = Random.Range(0, startingPositions.Length);
+    //    Transform.position = startingPositions[randStartingPos].position;
+    }
+}
